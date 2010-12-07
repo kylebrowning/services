cfbff621d26d3e50a02cf21c85c1210de1ac4a70
Kyle Browning <kyle@workhabit.com>
2010-12-07 23:49:35 +0000

    FIxes php 5.3 issues

--- servers/rest_server/lib/mimeparse.php
+++ servers/rest_server/lib/mimeparse.php
@@ -33,7 +33,7 @@ class Mimeparse {
      * @return array ($type, $subtype, $params)
      */
     public function parse_mime_type($mime_type) {
-      $parts = split(";", $mime_type);
+      $parts = explode(";", $mime_type);
 
       $params = array();
       foreach ($parts as $i=>$param) {
