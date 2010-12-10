9a3a50bc8b5c4b2e03a6dc7161d9f79433edac34
Kyle Browning <kyle@workhabit.com>
2010-12-10 03:10:27 +0000

    Updates 7.x branch to 3.x beta-1

---
+++ services.servers.api.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Services for the definition of servers.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Identifies a server implementation to Services.
+ *
+ * @return
+ *   An associative array with the following keys.
+ *
+ *   - name: The display name of this server.
+ */
+function hook_server_info() {
+  return array(
+    'name' => 'REST',
+  );  
+}
+
+/**
+ * Acts on requests to the server defined in hook_server_info().
+ *
+ * This is the main entry point to your server implementation.
+ * Need to get some more description about the best way to implement
+ * servers.
+ */
+function hook_server() {
+  
+}
