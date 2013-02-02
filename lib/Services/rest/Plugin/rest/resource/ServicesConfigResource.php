54fe2e88a45baef17a3a294a5599417a5ee9f273
marcingy <marcingy@gmail.com>
2013-02-01 17:51:39 -0700

    Continuing examination of core rest

--- lib/Services/rest/Plugin/rest/resource/ServicesConfigResource.php
+++ lib/Services/rest/Plugin/rest/resource/ServicesConfigResource.php
@@ -34,20 +34,54 @@ class ServicesConfigResource extends ResourceBase {
    *
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    */
-  public function get($name = NULL, $key = '') {
+  public function get($name = NULL) {
     if ($name) {
-      if ($config = config($name)) {
-        $value = $config->get($key);
+      // Core only supports routes with single /. In theory we could build
+      // routes if we could discover every single config in system and move to
+      // an approach similar to entities. This would then give :
+      // services/config/config_name/{id} where id is the name of the config
+      // item that is being updated. So the ability to affect multiple items
+      // would be gone and this might be correct.
+      $parameters = explode('::', $name);
+      if ($config = config($parameters[0])) {
+        $value = $config->get(!empty($parameters[1]) ? $parameters[1] : '');
         if (!empty($value)) {
-        // Serialization is done here, so we indicate with NULL that there is no
-        // subsequent serialization necessary.
-        $response = new ResourceResponse(NULL, 200, array('Content-Type' => 'application/vnd.drupal.ld+json'));
-        // @todo remove hard coded format here.
-        $response->setContent(drupal_json_encode($value));
-        return $response;
-      }
+          // Serialization is done here, so we indicate with NULL that there is no
+          // subsequent serialization necessary.
+          $response = new ResourceResponse(NULL, 200, array('Content-Type' => 'application/vnd.drupal.ld+json'));
+          // @todo remove hard coded format here.
+          $response->setContent(drupal_json_encode($value));
+          return $response;
+        }
       }
     }
     throw new NotFoundHttpException('Not Found');
+  }
+
+    /**
+   * Responds to config DELETE requests.
+   *
+   * @param mixed $name
+   *   The entity ID.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The HTTP response object.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   */
+  public function delete($id) {
+    $parameters = explode('::', $name);
+    if ($config = config($parameters[0])) {
+      if (!empty($parameters[1])) {
+        $config->clear($parameters[1]);
+      }
+      else {
+        $config->delete();
+      }
+      $config->save();
+      // Delete responses have an empty body.
+      return new ResourceResponse(NULL, 204);
+    }
+    throw new NotFoundHttpException(t('Config with name @name not found', array('@name' => $name)));
   }
 }
