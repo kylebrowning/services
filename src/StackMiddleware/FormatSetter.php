786011935a86f7c27a2ed8606edffe602ab9743b
Kris <kris@meridian-ds.com>
2015-09-17 17:22:42 -0500

    updating various documentation areas that were lacking or missing and adding an Accept-Encoding Vary statement to the final response object just to be very explicit with our returns.

--- src/StackMiddleware/FormatSetter.php
+++ src/StackMiddleware/FormatSetter.php
@@ -9,6 +9,15 @@ namespace Drupal\services\StackMiddleware;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
+/**
+ * Class which extracts accept headers and sets Request formats accordingly to
+ * at least allow for accept header variation when needed. This class respects
+ * core's usage of request format and attempts to communicate it generically.
+ * It should leave the Request format NULL when an unrecognized header is
+ * submitted. Drupal core will later set a default of html. If a recognized
+ * format comes through, it's set at this point to allow the PageCache
+ * http_middleware to properly cache by format.
+ */
 class FormatSetter implements HttpKernelInterface {
 
   /**
@@ -28,6 +37,9 @@ class FormatSetter implements HttpKernelInterface {
     $this->httpKernel = $http_kernel;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
     if ($request->headers->has('Accept')) {
       $request->setRequestFormat($request->getFormat($request->headers->get('Accept')));
