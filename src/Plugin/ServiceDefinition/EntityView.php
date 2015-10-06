b62c9a9bca686ba7e9bee8a13d5ef112d38ba98f
Kris Vanderwater <kris.vanderwater@MacBookAir-KrisVanderwater.local>
2015-10-06 11:16:32 -0400

    updated the EntityView service endpoint to properly render assets on the response. Huge thanks to tim.plunkett for having code that just did this already.

--- src/Plugin/ServiceDefinition/EntityView.php
+++ src/Plugin/ServiceDefinition/EntityView.php
@@ -5,6 +5,9 @@
  */
 
 namespace Drupal\services\Plugin\ServiceDefinition;
+use Drupal\Core\Asset\AssetCollectionRendererInterface;
+use Drupal\Core\Asset\AssetResolverInterface;
+use Drupal\Core\Asset\AttachedAssets;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\services\ServiceDefinitionBase;
@@ -35,7 +38,7 @@ class EntityView extends ServiceDefinitionBase implements ContainerFactoryPlugin
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('renderer'));
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('renderer'), $container->get('asset.resolver'), $container->get('asset.css.collection_renderer'), $container->get('asset.js.collection_renderer'));
   }
 
   /**
@@ -44,9 +47,12 @@ class EntityView extends ServiceDefinitionBase implements ContainerFactoryPlugin
    * @param mixed $plugin_definition
    * @param RendererInterface $renderer
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, RendererInterface $renderer) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RendererInterface $renderer, AssetResolverInterface $asset_resolver, AssetCollectionRendererInterface $css_collection_renderer, AssetCollectionRendererInterface $js_collection_renderer) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->renderer = $renderer;
+    $this->assetResolver = $asset_resolver;
+    $this->cssCollectionRenderer = $css_collection_renderer;
+    $this->jsCollectionRenderer = $js_collection_renderer;
   }
 
 
@@ -61,11 +67,58 @@ class EntityView extends ServiceDefinitionBase implements ContainerFactoryPlugin
     /** @var $entity \Drupal\Core\Entity\EntityInterface */
     $entity = $this->getContextValue('entity');
     $view_builder = \Drupal::entityManager()->getViewBuilder($entity->getEntityTypeId());
-    $elements = $view_builder->view($entity, $view_mode);
-    return [
-      'render_array' => $elements,
-      'data' => $this->renderer->renderRoot($elements)
-    ];
+    $render_array = $view_builder->view($entity, $view_mode);
+
+    $result = [];
+    $result['body'] = $this->renderer->renderRoot($render_array);
+    $all_assets = $this->gatherAssetMarkup($render_array);
+    $result += $this->renderAssets($all_assets);
+
+    return $result;
+  }
+
+  /**
+   * Renders an array of assets.
+   *
+   * @param array $all_assets
+   *   An array of all unrendered assets keyed by type.
+   *
+   * @return array
+   *   An array of all rendered assets keyed by type.
+   */
+  protected function renderAssets(array $all_assets) {
+    $result = [];
+    foreach ($all_assets as $asset_type => $assets) {
+      $result[$asset_type] = [];
+      foreach ($assets as $asset) {
+        $result[$asset_type][] = $this->renderer->renderRoot($asset);
+      }
+    }
+    return $result;
+  }
+  /**
+   * Gathers the markup for each type of asset.
+   *
+   * @param array $render_array
+   *   The render array for the entity.
+   *
+   * @return array
+   *   An array of rendered assets. See self::getRenderedEntity() for the keys.
+   *
+   * @todo template_preprocess_html() should be split up and made reusable.
+   */
+  protected function gatherAssetMarkup(array $render_array) {
+    $assets = AttachedAssets::createFromRenderArray($render_array);
+    // Render the asset collections.
+    $css_assets = $this->assetResolver->getCssAssets($assets, FALSE);
+    $variables['styles'] = $this->cssCollectionRenderer->render($css_assets);
+    list($js_assets_header, $js_assets_footer) = $this->assetResolver->getJsAssets($assets, FALSE);
+    $variables['scripts'] = $this->jsCollectionRenderer->render($js_assets_header);
+    $variables['scripts_bottom'] = $this->jsCollectionRenderer->render($js_assets_footer);
+    // Handle all non-asset attachments.
+    drupal_process_attached($render_array);
+    $variables['head'] = drupal_get_html_head(FALSE);
+    return $variables;
   }
 
 }
