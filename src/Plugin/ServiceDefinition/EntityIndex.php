6c0d3aa452de341c2a0fe6ca8e75f2b2e7416ba4
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2016-01-14 12:41:19 -0800

    Issue #2650532 by phenaproxima: EntityIndex should return entity labels

--- src/Plugin/ServiceDefinition/EntityIndex.php
+++ src/Plugin/ServiceDefinition/EntityIndex.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\services\Plugin\ServiceDefinition;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\services\ServiceDefinitionBase;
@@ -28,26 +29,43 @@ use Drupal\Core\Routing\RouteMatchInterface;
 class EntityIndex extends ServiceDefinitionBase implements ContainerFactoryPluginInterface {
 
   /**
-   * @var QueryFactoryInterface
+   * The entity query factory.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactoryInterface
    */
   protected $queryFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity.query'));
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.query'),
+      $container->get('entity_type.manager')
+    );
   }
 
   /**
    * @param array $configuration
    * @param string $plugin_id
    * @param mixed $plugin_definition
-   * @param QueryFactory $query_factory
+   * @param \Drupal\Core\Entity\Query\QueryFactory $query_factory
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryFactory $query_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryFactory $query_factory, EntityTypeManagerInterface $entity_type_manager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->queryFactory = $query_factory;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -63,9 +81,16 @@ class EntityIndex extends ServiceDefinitionBase implements ContainerFactoryPlugi
     if ($request->query->has('limit') && is_numeric($request->query->get('limit'))) {
       $limit = $request->query->get('limit');
     }
-    return $this->queryFactory->get($entity_type_id, 'AND')
+    $result = $this->queryFactory->get($entity_type_id, 'AND')
       ->range($start, $limit)
       ->execute();
+
+    $map_function = function ($id) use ($entity_type_id) {
+      return $this->entityTypeManager->getStorage($entity_type_id)
+        ->load($id)
+        ->label();
+    };
+    return array_map($map_function, $result);
   }
 
 }
