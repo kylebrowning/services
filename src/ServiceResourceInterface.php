c49707f799af6fc8852b57d53c0335572692148c
Travis Tomka <underoathosx@gmail.com>
2016-08-24 15:25:04 -0600

    Implement service resource config entity.

---
+++ src/ServiceResourceInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\services;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Interface \Drupal\services\Entity\ServiceResourceInterface.
+ */
+interface ServiceResourceInterface extends ConfigEntityInterface {
+
+  /**
+   * Get resource allowed formats.
+   *
+   * @return array
+   *   An array of allowed formats.
+   */
+  public function getFormats();
+
+  /**
+   * Get resource allowed authentication.
+   *
+   * @return array
+   *   An array of allowed authentication.
+   */
+  public function getAuthentication();
+
+}
